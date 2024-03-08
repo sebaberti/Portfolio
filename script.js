f0b00da449fb4331b2669dcf54bc7b42532b9ecb
Sebastian Berti <sebaberti.8@gmail.com>
2024-03-08 17:24:46 -0300

    correccion de errores

--- script.js
+++ script.js
@@ -24,22 +24,7 @@ function responsiveMenu() {
     }
 }
 
-//detecto el scrolling para aplicar la animación del la barra de habilidades
-window.onscroll = function() { efectoHabilidades() };
-
-//funcion que aplica la animación de la barra de habilidades
-function efectoHabilidades() {
-    var skills = document.getElementById("skills");
-    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
-    if (distancia_skills >= 300) {
-        document.getElementById("html").classList.add("barra-progreso1");
-        document.getElementById("js").classList.add("barra-progreso2");
-        document.getElementById("bd").classList.add("barra-progreso3");
-        document.getElementById("ps").classList.add("barra-progreso4");
-    }
-
-}
-
+//funcion para que puedan copiar mi mail con un click 
 document.getElementById('copy-email').addEventListener('click', function() {
     var email = 'sebaberti.8@gmail.com';
     var tempInput = document.createElement('input');
