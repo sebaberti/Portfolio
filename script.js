579090f6578e4cd5d2dc23368d7f9028528b2fe1
Sebastian Berti <sebaberti.8@gmail.com>
2024-07-14 19:05:27 -0300

    se agrego proyectos y se modifico colores

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 //Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+// function seleccionar(link) {
+//     var opciones = document.querySelectorAll('#links  a');
+//     opciones[0].className = "";
+//     opciones[1].className = "";
+//     opciones[2].className = "";
+//     opciones[3].className = "";
+//     opciones[4].className = "";
+//     link.className = "seleccionado";
+
+//     //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+//     //en modo responsive
+//     var x = document.getElementById("nav");
+//     x.className = "";
+// }
+
+
+
 function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
+    var opciones = document.querySelectorAll('#links a');
+    opciones.forEach(opcion => opcion.className = "");
     link.className = "seleccionado";
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
+    // Hacemos desaparecer el menú una vez que se ha seleccionado una opción en modo responsive
+    var nav = document.getElementById("nav");
+    if (nav.className === "responsive") {
+        nav.className = "";
+    }
+
     var x = document.getElementById("nav");
     x.className = "";
 }
-
 //función que muestra el menu responsive
 function responsiveMenu() {
     var x = document.getElementById("nav");
